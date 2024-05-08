9d1767fcc83caa1184f959661e64eb7e95c5e8d0
everGreenGH <skin0011@naver.com>
2024-05-08 22:27:06 +0900

    Chore: Export provider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import client from "./browser-client-factory";
 export { createInstance } from "./browser-client-factory";
 export const { init, addReferral } = client;
+export { SplitBrowserProvider, useSplitBrowser } from "./BrowserProvider";
