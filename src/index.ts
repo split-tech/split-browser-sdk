d373c8e4ec37eeadfcf54bc7c2fa4d652a72d892
miju3545 <miju3545@gmail.com>
2024-05-09 17:11:32 +0900

    Fix: Export types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import client from "./browser-client-factory";
 export { createInstance } from "./browser-client-factory";
 export const { init, addReferral } = client;
 export { SplitBrowserProvider, useSplitBrowser } from "./BrowserProvider";
-export type { SplitProviderConfig } from "./BrowserProvider";
 export type { SplitConfig } from "./browser-client";
+export type { SplitProviderConfig } from "./BrowserProvider";
